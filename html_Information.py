d7d4f051382e9b9d9b417f138fa8ba9982105c0b
MalyshevaAN <st098062@student.spbu.ru>
2022-04-15 14:02:25 +0300

    немного доделала задачу про википедию

--- html_Information.py
+++ html_Information.py
@@ -1,6 +1,7 @@
 import requests
 
-import re
+
+from bs4.element import Tag
 
 from bs4 import BeautifulSoup
 
@@ -8,7 +9,7 @@ from bs4 import BeautifulSoup
 def name_of_course(link, year, season):
     l = link + '/' + str(year) + season + '/'
     response = requests.get(l).text
-    document = BeautifulSoup(response)
+    document = BeautifulSoup(response, features = 'html.parser')
     section = document.find('section', {'class': 'main-content'})
     ul = section.find('ul')
     ul_in_ul = ul.find_all('ul')
@@ -24,7 +25,7 @@ def name_of_course(link, year, season):
 def class_of_animal(link, animal):
     l = link + '/' + animal
     response = requests.get(l).text
-    document = BeautifulSoup(response)
+    document = BeautifulSoup(response, features = 'html.parser')
     table = document.find('table')
     td = table.find('td', {'class': 'plainlist'})
     div = td.find_all('div', {'class': 'ts-Taxonomy-rang-row'})
@@ -34,33 +35,40 @@ def class_of_animal(link, animal):
             return d[1].text
 
 
-def link_from_wikipedia(link):
-    global s
 
-    ans = ''
-    while len(s) != 100 and link not in s:
+
+def link_from_wikipedia(link, s,names):
+    l = link
+    print(l)
+    if len(s) == 100 or l in s:
+        return l
+    else:
+
         response = requests.get(link).text
-        document = BeautifulSoup(response)
-        p = document.find('p')
-        span = p.find('span')
-        a2 = []
-        if span:
-            a2 = span.find_all('a')
-        a = p.find_all('a')
-        reg = re.compile(r'\(.*\)')
-        languages = []
-        if re.findall(reg, str(p)):
-            for elem in re.findall(reg, str(p)):
-                x = BeautifulSoup(elem[1: len(elem) - 1])
-                languages.extend(x.find_all('a'))
-        for elem in a:
-            if elem not in a2 and elem not in languages:
-                reg2 = re.compile(r'/wiki/.[^\s]*')
-                ans = 'https://ru.wikipedia.org' + re.findall(reg2, str(elem))[0]
-                s.add(link)
-                s.add(ans)
-                link_from_wikipedia(ans)
-    return ans
+        document = BeautifulSoup(response, features='html.parser')
+        div = document.find('div', {'class': 'mw-parser-output'})
+        if not div:
+            print(1)
+            return l
+        p = div.find('p', recursive=False)
+        if not p:
+            print(2)
+            return l
+        flag = 1
+        for child in p.children:
+            if '(' in str(child):
+                flag = 0
+            if ')' in str(child):
+                flag = 1
+            if flag == 1 and isinstance(child, Tag) and str(child).startswith('<a'):
+                s.append(link)
+                new = 'https://ru.wikipedia.org' + str(child.get('href'))
+                names.append(str(child.get('title')))
+                link_from_wikipedia(new, s, names)
+                break
+        print(3)
+        return l
+
 
 
 link = 'http://students.iposov.spb.ru'
@@ -70,7 +78,7 @@ print(name_of_course(link, year, season))
 wiki = 'https://ru.wikipedia.org/wiki'
 animal = 'Зебры'
 print(class_of_animal(wiki, animal))
-s = set()
+s = []
+names = []
 link_wiki = 'https://ru.wikipedia.org/wiki/%D0%9E%D1%81%D1%8C%D0%BC%D0%B8%D0%BD%D0%BE%D0%B3%D0%B8'
-print(link_from_wikipedia((link_wiki)))
-
+print(link_from_wikipedia(link_wiki, s, names))
